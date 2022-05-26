61843f9c3d65d416ebe532db1442b781c24fc4c8
Andrius <andriukas.tumsys@gmail.com>
2022-05-26 17:27:43 +0300

    Add payments

---
+++ foodkept/Pages/Payments/Create.cshtml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using FoodKept.Data;
+using FoodKept.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.ComponentModel.DataAnnotations;
+using FoodKept.Extensions;
+using FoodKept.ViewModels;
+
+namespace FoodKept.Pages.Payments
+{
+    [Authorize(Roles = "Admin, Restaurant")]
+    public class CreateModel : PageModel
+    {
+        private readonly FoodKept.Data.ShopContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CreateModel(FoodKept.Data.ShopContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+        public Food Food { get; set; }
+        [BindProperty]
+        [Display(Name = "Image")]
+        //[Required(ErrorMessage = "Pick an Image")]
+        [AllowedImgExtensions(new string[] { ".jpg", ".jpeg", ".png" })]
+        public IFormFile FoodImage { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        [BindProperty]
+        public List<SelectListItem> EnumCategories { get; set; }
+
+        public IActionResult OnGet()
+        {
+            EnumCategories = Enum.GetValues(typeof(FoodCategories.Category)).Cast<FoodCategories.Category>().Select(v => new SelectListItem
+            {
+                Text = v.ToString(),
+                Value = v.ToString()
+            }).ToList();
+
+            return Page();
+        }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            //If the user creates a food, that food is linked to the user by id
+            Food.ApplicationUserId = _userManager.GetUserId(User);
+
+            if (FoodImage != null)
+            {
+                //Convert image file to byte array and add to model
+                Food.FoodImage = GetByteArrayFromImage(FoodImage);
+            }
+
+            //Initialize CurrentPrice
+            Food.CurrentPrice = new CurrentPrice();
+
+            //Get current application user restaurant name
+            ApplicationUser applicationUser = await _userManager.GetUserAsync(User);
+            Food.RestaurantName = applicationUser.RestaurantName;
+
+            //Initialize to DB
+            _context.FoodData.Add(Food);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+
+        private static byte[] GetByteArrayFromImage(IFormFile file)
+        {
+            using (var target = new MemoryStream())
+            {
+                file.CopyTo(target);
+                return target.ToArray();
+            }
+        }
+    }
+}
---
+++ foodkept/Pages/Payments/Payments.cshtml.cs
@@ -0,0 +1,247 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FoodKept.Data;
+using FoodKept.Helpers;
+using FoodKept.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodKept.Pages.Payments
+{
+    [Authorize(Roles = "Customer, Admin")]
+    public class CartModel : PageModel
+    {
+        public IList<ShoppingCart> Cart { get; set; }
+        private readonly ShopContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IWebHostEnvironment _environment;
+
+        public CartModel(ShopContext context, UserManager<ApplicationUser> userManager, IWebHostEnvironment environment)
+        {
+            _context = context;
+            _userManager = userManager;
+            _environment = environment;
+        }
+
+
+        public void OnGet()
+        {
+            var userId = _userManager.GetUserId(User);
+            Cart = _context.Cart.Include(c => c.Food)
+                .Include(c => c.Food.CurrentPrice)
+                .Include(c => c.Food.ApplicationUser)
+                .Include(c => c.Food.DiscountList)
+                .Where(c => (c.ApplicationUserId == userId) && (c.Reserved == false)).ToList();
+
+            foreach (var cartItem in Cart)
+            {
+                CalculateCurrentPrice.CalculatePriceForFood(cartItem.Food);
+            }
+        }
+
+
+        public async Task<IActionResult> OnPostAddToCart(string id)
+        {
+            var cartt = Cart;
+            Food food = _context.FoodData.FirstOrDefault(db => db.ID.ToString() == id);
+            ShoppingCart result = _context.Cart.FirstOrDefault(c =>
+                    c.ApplicationUserId == _userManager.GetUserId(User) &&
+                    c.FoodId == food.ID &&
+                    c.Reserved == false);
+
+            if (result != null)
+            {
+                result.Quantity = (result.Quantity < food.Quantity) ? result.Quantity + 1 : food.Quantity;
+                _context.Attach(result).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                
+                return new JsonResult(result.Quantity);
+            }
+            else
+            {
+                ShoppingCart cart = new ShoppingCart
+                {
+                    FoodId = food.ID,
+                    ApplicationUserId = _userManager.GetUserId(User),
+                    Quantity = 1,
+                    Reserved = false,
+                    Customer = await _userManager.GetUserAsync(User)
+                };
+
+                _context.Cart.Add(cart);
+                await _context.SaveChangesAsync();
+
+                return new JsonResult(cart.Quantity);
+            }
+
+        }
+        public async Task<IActionResult> OnPostRemoveFromCartAsync(int id)
+        {
+            var cart = _context.Cart.FirstOrDefault(op => op.Id == id);
+
+            if (cart != null)
+            {
+                _context.Cart.Remove(cart);
+                await _context.SaveChangesAsync();
+            }
+            OnGet();
+            return Page();
+        }
+
+
+        public async Task <IActionResult> OnPostReserve()
+        {
+            OnGet();
+            foreach (var item in Cart)
+            {
+                var food = item.Food;
+                food.Quantity = item.Food.Quantity - item.Quantity;
+                item.Reserved = true;
+                _context.Attach(food).State = EntityState.Modified;
+                _context.Attach(item).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Credentials = new System.Net.NetworkCredential("foodkepterino@gmail.com", "foodkept4");
+
+            var user = _userManager.GetUserAsync(User).Result;
+
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.EnableSsl = true;
+            MailMessage mail = new MailMessage();
+            mail.IsBodyHtml = true;
+            mail.From = new MailAddress("foodkepterino@gmail.com", "FoodKept");
+            mail.To.Add(new MailAddress(user.Email));
+            mail.Subject = user.FirstName + " reservation"; 
+            smtpClient.Port = 587;
+            smtpClient.Host = "smtp.gmail.com";
+
+            string path = Path.Combine(path1: _environment.ContentRootPath, path2: "App_Data\\emailTemplate.txt");
+
+            var foodsInfo = _context.FoodData.ToList().Join(
+                Cart,
+                food => food.ID, crt => crt.FoodId,
+                (food, crt) => new
+                {
+                    foodId = food.ID,
+                    foodName = food.FoodName,
+                    quantity = crt.Quantity,
+                    foodsOwner = food.ApplicationUserId
+                }).ToList();
+
+            // group join
+            var infoForCustomer = _context.ApplicationUsers.ToList().GroupJoin(
+                foodsInfo,
+                appUser => appUser.Id, foodInfo => foodInfo.foodsOwner,
+                (appUser, collection) => new
+                {
+                    Restaurant = appUser.RestaurantName,
+                    Country = appUser.Country,
+                    City = appUser.City,
+                    Address = appUser.Address,
+                    collection = collection
+                }).ToList();
+
+
+            // named argument usage
+            string message = ReadFromFile(filePath: path);
+            foreach(var restaurants in infoForCustomer)
+            {
+                if (!restaurants.collection.Any()) continue;
+                message +=
+                    $"<p>{restaurants.Restaurant}</p>" +
+                    $"<p1>Adress: {restaurants.Address}, {restaurants.City}    {restaurants.Country}</p1><br />";
+
+                foreach (var foods in restaurants.collection)
+                {
+                    message += $"<label>Meal: {foods.foodName}  ||  Quantity: {foods.quantity}</label><br />";
+                }
+                message += "<br />";
+            }
+
+            mail.Body = message;
+            try
+            {
+                smtpClient.Send(message: mail);
+            }
+            catch (SmtpFailedRecipientsException ex)
+            {
+                SmtpStatusCode status = ex.InnerExceptions[0].StatusCode;
+
+                if (status == SmtpStatusCode.MailboxBusy || status == SmtpStatusCode.MailboxUnavailable)
+                {
+                    ViewData["Message"] = string.Format("Delivery failed - retrying again");
+                    Thread.Sleep(1000);
+                    smtpClient.Send(mail);
+                }
+                else
+                {
+                    ViewData["Message"] = string.Format("Failed to deliver message to {0}", ex.InnerExceptions[0].FailedRecipient);
+                }
+                
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                ViewData["Message"] = string.Format("Unable to send reservation to your email. Please try again. Error: {0}", ex.ToString());
+                return Page();
+            }
+
+            OnGet();
+            ViewData["Message"] = string.Format("Succesfully reserved your order!");
+
+            return Page();
+        }
+
+
+        private static string ReadFromFile(string filePath)
+        {
+            string message;
+            try
+            {
+                StreamReader sr = new StreamReader(filePath);
+                message = sr.ReadLine();
+                string newLine;
+                while((newLine = sr.ReadLine()) != null)
+                {
+                    message += newLine;
+                }
+                sr.Close();
+            }
+            catch(Exception)
+            {
+                return "";
+            }
+            return message;
+        }
+
+        public async Task<IActionResult> OnPostChangeQuantity(int minus_plus, int id)
+        {
+            var cart = _context.Cart.FirstOrDefault(op => op.Id == id);
+            if (cart != null)
+            {
+                cart.Quantity = minus_plus;
+                _context.Attach(cart).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+            //var cart = context.Cart.FirstOrDefault(op => op.Id)
+            return new JsonResult(new {
+                minus_plus = minus_plus,
+                id = id });
+        }
+    }
+
+}
+
